f050df05fe74d7e8a427d57975545383e5880477
anishabrahma <103175159+anishabrahma@users.noreply.github.com>
2024-07-22 17:58:14 +0530

    Add files via upload

--- list.js
+++ list.js
@@ -1,19 +1,26 @@
 import React, { useState } from 'react';
+import Form from './form'; 
+import Details from './details'; 
 
 function List() {
     const [list, setList] = useState([]);
+    const [selected, setSelected] = useState(null);
 
     const adddata = (obj) => {
         setList([...list, obj]);
     };
 
     return (
-        <div>
+        <div className="m-2 p-2" style={{ border: '2px solid green' }}>
             <ul>
                 {list.map((item) => (
-                    <li key={item.id}>{item.name}</li>
+                    <li key={item.id} onClick={() => setSelected(item)}>
+                        {item.name}
+                    </li>
                 ))}
             </ul>
+            <Form adddata={adddata} />
+            {selected && <Details data={selected} />}
         </div>
     );
 }
